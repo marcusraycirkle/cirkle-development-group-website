58a8867ef57ccb620928ea84d484cc207a9152db
marcusraycirkle <marcusray@cirkledevelopment.co.uk>
2025-10-07 18:20:42 +0000

    edited website for a better fixed look

--- js/script.js
+++ js/script.js
@@ -12,14 +12,21 @@ function showPage(pageId) {
   window.location.hash = pageId;
 }
 
-// On load, show page based on hash or default to home
 window.addEventListener('load', () => {
   const hash = window.location.hash.substring(1);
   showPage(hash || 'home');
 });
 
-// Handle hash changes (back/forward buttons)
 window.addEventListener('hashchange', () => {
   const hash = window.location.hash.substring(1);
   showPage(hash || 'home');
+});
+
+window.addEventListener('scroll', () => {
+  const header = document.querySelector('header');
+  if (window.scrollY > 50) {
+    header.classList.add('scrolled');
+  } else {
+    header.classList.remove('scrolled');
+  }
 });
